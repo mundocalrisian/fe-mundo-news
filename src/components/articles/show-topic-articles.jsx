6adab666f76f5cfa6591fd39f95da4bf8d76eb74
mundocalrisian <greg.munden@gmail.com>
2024-03-14 16:57:25 +0000

    implement sort by for topics. Still needs functionality for all articles though

--- src/components/articles/show-topic-articles.jsx
+++ src/components/articles/show-topic-articles.jsx
@@ -3,20 +3,69 @@ import { getTopicArticles } from "../../utils/api";
 import { ShowArticleCard } from "./show-article-card";
 import { useParams } from "react-router-dom";
 import { ShareButtons } from "../share/share-buttons";
+import { SortArticles } from "./sort-articles";
+import Select from 'react-select'
+import { sortByCommentCountAsc, sortByCommentCountDesc, sortByCreatedAsc, sortByCreatedDesc, sortByVotesAsc, sortByVotesDesc } from "../../utils/sort-by"
 
 export default function ShowTopicArticles ({allTopics}) {
     const [topicArticles, setTopicArticles] = useState([])
     const [isFetching, setIsFetching] = useState(true)
+    const [sortByOption, setSortByOption] = useState({value: 'DateDesc', label: 'Date created (Descending)'})
+    // console.log(sortByOption, "sortby");
 
     const topic = useParams().topic
+
+    const options = [
+        {value:"DateAsc", label: "Date created (Ascending)"}, 
+        {value:"DateDesc", label: "Date created (Descending)"}, 
+        {value:"CommentsAsc", label: "Number of Comments (Ascending)"}, 
+        {value:"CommentsDesc", label: "Number of Comments (Descending)"}, 
+        {value:"VotesAsc", label: "Number of Votes (Ascending)"}, 
+        {value:"VotesDesc", label: "Number of Votes (Descending)"}
+    ]
     
     useEffect(() => {
         getTopicArticles(topic)
         .then((data) => {
+            data.forEach((article) => {
+            article.created_at = Date.parse(article.created_at)
+            })
             setTopicArticles(data)
             setIsFetching(false)
         })
     }, [topic])
+
+    useEffect(() => {
+
+        switch (sortByOption.value) {
+            case "DateAsc":
+                const createdAsc = sortByCreatedAsc(topicArticles)
+                setTopicArticles(createdAsc)
+                break;
+            case "DateDesc":
+                const createdDesc = sortByCreatedDesc(topicArticles)
+                setTopicArticles(createdDesc)
+                break;
+            case "CommentsAsc":
+                const commentCountAsc = sortByCommentCountAsc(topicArticles)
+                setTopicArticles(commentCountAsc)
+                break;
+            case "CommentsDesc":
+                const commentCountDesc = sortByCommentCountDesc(topicArticles)
+                setTopicArticles(commentCountDesc)
+                break;
+            case "VotesAsc":
+                const votesAsc = sortByVotesAsc(topicArticles)
+                setTopicArticles(votesAsc)
+                break;
+            case "VotesDesc":
+                const votesDesc = sortByVotesDesc(topicArticles)
+                setTopicArticles(votesDesc)
+                break;
+            
+        }
+    }, [sortByOption])
+
 
     if (isFetching) {
         return (
@@ -27,28 +76,22 @@ export default function ShowTopicArticles ({allTopics}) {
     } else {
         return (
             <section className="articles-container">
-                <select name="Sort By" id="">
-                    <option value="Topic">Date (Ascending)</option>
-                    <option value="Topic">Date (Descending)</option>
-                    <option value="Topic">Comment Count (Ascending)</option>
-                    <option value="Topic">Comment Count (Descending)</option>
-                    <option value="Topic">Votes (Ascending)</option>
-                    <option value="Topic">Votes (Descending)</option>
-                </select>
+                <Select 
+                    className="sort-by-dropdown" 
+                    options={options}
+                    defaultValue={sortByOption}
+                    onChange={setSortByOption}
+                />
                 {/* <ShareButtons /> */}
                 {topicArticles.map((article) => {
                     return (
                         <div key={article.article_id} className="article-card">
-                        <ShowArticleCard article={article}/>
-                    </div>
-                )
-            })}
+                            <ShowArticleCard article={article}/>
+                        </div>
+                    )
+                })}
             </section>
         )
     }
-    
-
-
-
 
 }
--- src/components/share/share-buttons.jsx
+++ src/components/share/share-buttons.jsx
@@ -1,50 +1,79 @@
-// import ReactDOM from "react-dom";
-// import  findDOMNode  from "react-dom";
 import "./share-buttons.css"
+import {EmailShareButton,
+    FacebookShareButton,
+    GabShareButton,
+    HatenaShareButton,
+    InstapaperShareButton,
+    LineShareButton,
+    LinkedinShareButton,
+    LivejournalShareButton,
+    MailruShareButton,
+    OKShareButton,
+    PinterestShareButton,
+    PocketShareButton,
+    RedditShareButton,
+    TelegramShareButton,
+    TumblrShareButton,
+    TwitterShareButton,
+    ViberShareButton,
+    VKShareButton,
+    WhatsappShareButton,
+    WorkplaceShareButton,
+} from "react-share"
+import {
+    EmailIcon,
+    FacebookIcon,
+    FacebookMessengerIcon,
+    GabIcon,
+    HatenaIcon,
+    InstapaperIcon,
+    LineIcon,
+    LinkedinIcon,
+    LivejournalIcon,
+    MailruIcon,
+    OKIcon,
+    PinterestIcon,
+    PocketIcon,
+    RedditIcon,
+    TelegramIcon,
+    TumblrIcon,
+    TwitterIcon,
+    ViberIcon,
+    VKIcon,
+    WeiboIcon,
+    WhatsappIcon,
+    WorkplaceIcon,
+    XIcon,
+    } from "react-share";
+
 
 export function ShareButtons () {
 
     const url = window.location.href
-    // console.log(window.location.href, "window ref");
-    // console.log(encodeURIComponent(url));
-
-    // const a = ReactDOM.findDOMNode(document.getElementsByClassName('.share-button'))
-    // console.log(a);
-
-    // switch () {
-    //     case 'email':
-    //     shareUrl = ``;
-    //     break;
-    //     case 'facebook':
-    //     shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
-    //     break;
-    //     case 'instagram':
-    //     shareUrl = `https://reddit.com/submit?url=${encodeURIComponent(url)}`;
-    //     break;
-    //     case 'twitter':
-    //     shareUrl = `https://twitter.com/share?url=${encodeURIComponent(url)}`;
-    //     break;
-    //     case 'linkedin':
-    //     shareUrl = `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(url)}`;
-    //     break;
-    //     case 'whatsapp':
-    //     shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(url)}`;
-    //     break;
+   
+    // function handleShare () {
+    //     return window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`, '_blank')
     // }
 
-    function handleShare () {
-        return window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`, '_blank')
-    }
-
     return (
-        <div className="share-buttons">
-                    <button className="share-button email"><i className="fa-regular fa-envelope"></i></button>
-                    <button className="share-button whatsapp"><i className="fa-brands fa-whatsapp"></i></button>
-                    <button onClick={handleShare()} className="share-button facebook"><i className="fa-brands fa-facebook"></i></button>
-                    <button className="share-button instagram"><i className="fa-brands fa-instagram"></i></button>
-                    <button className="share-button twitter"><i className="fa-brands fa-x-twitter"></i></button>
-                    <button className="share-button linkedin"><i className="fa-brands fa-linkedin"></i></button>
-                    <button className="share-button copy"><i className="fa-regular fa-copy"></i></button>
-                </div>
+        <>
+            <div className="react-share-buttons">
+            <EmailShareButton url={url}>
+                <EmailIcon size={32} round={true}/>
+            </EmailShareButton>
+            <TwitterShareButton>
+                <XIcon size={32} round={true}/>
+            </TwitterShareButton>
+            </div>
+        {/* <div className="share-buttons">
+            <button className="share-button email"><i className="fa-regular fa-envelope"></i></button>
+            <button className="share-button whatsapp"><i className="fa-brands fa-whatsapp"></i></button>
+            <button onClick={handleShare()} className="share-button facebook"><i className="fa-brands fa-facebook"></i></button>
+            <button className="share-button instagram"><i className="fa-brands fa-instagram"></i></button>
+            <button className="share-button twitter"><i className="fa-brands fa-x-twitter"></i></button>
+            <button className="share-button linkedin"><i className="fa-brands fa-linkedin"></i></button>
+            <button className="share-button copy"><i className="fa-regular fa-copy"></i></button>
+        </div> */}
+        </>
     )
 }
